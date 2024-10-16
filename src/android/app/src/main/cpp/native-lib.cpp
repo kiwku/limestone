8634b79ad8277f532a89d86d3974462f671d5e06
Kiwku <xkivinum@gmail.com>
2024-10-16 21:51:04 +0200

    some android work (most likely android will be done last)

--- src/android/app/src/main/cpp/native-lib.cpp
+++ src/android/app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,11 @@
 #include <jni.h>
 #include <string>
 
+#include <fmt/color.h>
+#include <fmt/core.h>
+#include <fmt/ranges.h>
+
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_kiwku_limestone_MainActivity_stringFromJNI(
         JNIEnv* env,
